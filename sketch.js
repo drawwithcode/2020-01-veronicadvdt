ef17f2ea2cf7b6c9d7f73f9db3e942f39e7e4fa6
veronicadvdt <veronicadetoni@gmail.com>
2020-09-30 00:23:37 +0200

    Generative_Artwork_V003

--- sketch.js
+++ sketch.js
@@ -6,42 +6,89 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   background(224,224,235);
   angleMode(DEGREES);
-  frameRate(20);
+  frameRate(30);
 }
 
 function draw() {
+  //whitecircleFill
+  push();
+  noStroke();
+  fill("white");
+  translate(width/2,height/2);
+  ellipse(0, 0, 30);
+  pop();
 
-  pop()
+  //whitecircleStroke
+  push();
+  stroke("white");
+  strokeWeight(1);
+  noFill()
+  translate(width/2,height/2);
+  ellipse(0, 0, 110);
+  pop();
 
+  // LineRed45
+  push();
+  noStroke();
+  fill(lerpColor(color("red"),color("white"),frameCount/100));
+  translate(width/2,height/2);
+  rotate(45,0);
+  translate(0,frameCount*(2));
+  ellipse(0, 0, 30);
+  pop();
+
+  // LineYellow
+  push();
+  noStroke();
+  fill(lerpColor(color("yellow"),color("white"),frameCount/100));
+  translate(width/2,height/2);
+  translate(0,frameCount*(-2));
+  ellipse(0, 0, 30);
+  rotate(frameCount);
+  pop();
+
+  // LineBlue-45
+  push();
+  noStroke();
+  fill(lerpColor(color("blue"),color("white"),frameCount/100));
+  translate(width/2,height/2);
+  rotate(-45,0);
+  translate(0,frameCount*2);
+  ellipse(0, 0, 30);
+  pop();
+
+  // 1.CircleYellow
   push();
   noStroke();
   fill(lerpColor(color("yellow"),color("white"),frameCount/120));
   translate(width/2,height/2);
-  rotate(frameCount* 3);
-  translate(100,0);
-  ellipse(0, 0, 20);
-  pop();
-
-
-
-  push();
-  noStroke();
-  fill(lerpColor(color("blue"),color("white"),frameCount/120));
-  translate(width/2,height/2);
-  rotate(frameCount* -2);
+  rotate(frameCount*2);
   translate(150,0);
   ellipse(0, 0, 20);
   pop();
 
+  // 2.CircleBlue
   push();
   noStroke();
-  fill(lerpColor(color("red"),color("white"),frameCount/120));
+  fill(lerpColor(color("blue"),color("white"),frameCount/120));
   translate(width/2,height/2);
-  rotate(frameCount* 2);
+  rotate(-frameCount*2);
   translate(200,0);
   ellipse(0, 0, 20);
   pop();
 
+  // 3.CircleRed
+  push();
+
+  noStroke();
+  fill(lerpColor(color("red"),color("white"),frameCount/120));
+  translate(width/2,height/2);
+  rotate(frameCount*2);
+  translate(250,0);
+  ellipse(0, 0, 20);
+  pop();
+
+  // DotCircle
   push();
   noStroke();
   fill("white");
@@ -51,6 +98,8 @@ function draw() {
   ellipse(0, 0, 10);
   pop();
 
+
+  // DotCircle*2
   push();
   noStroke();
   fill("white");
@@ -60,7 +109,7 @@ function draw() {
   ellipse(0, 0, 10);
   pop();
 
-  if(frameCount == 360){noLoop()};
+  if(frameCount ==400){noLoop()};
 
 
 
